25ca783d46a2ed7585351760bfed6732d8211068
Lawrence Cherone <lcherone@users.noreply.github.com>
2018-03-03 11:17:35 +0000

    Update start.php

--- start.php
+++ start.php
@@ -31,8 +31,7 @@ $worker = new Worker("Websocket://0.0.0.0:7778");
 $worker->name = 'phptty';
 $worker->user = USER;
 
-$worker->onConnect = function($connection)
-{
+$worker->onConnect = function ($connection) {
     //To do this, PHP_CAN_DO_PTS must be enabled. See ext/standard/proc_open.c in PHP directory.
     /*$descriptorspec = array(
         0 => array('pty'),
@@ -42,45 +41,45 @@ $worker->onConnect = function($connection)
     
     //Pipe can not do PTY. Thus, many features of PTY can not be used.
     //e.g. sudo, w3m, luit, all C programs using termios.h, etc.
-    $descriptorspec = array(
-        0=>array("pipe", "r"),
-        1=>array("pipe", "w"),
-        2=>array("pipe", "w")
-    );
+    $descriptorspec = [
+        0 => ["pipe", "r"],
+        1 => ["pipe", "w"],
+        2 => ["pipe", "w"]
+    ];
     
     unset($_SERVER['argv']);
+    
     $env = array_merge(
-        array('COLUMNS'=>130, 'LINES'=> 50), $_SERVER
+        ['COLUMNS' => 130, 'LINES' => 50], $_SERVER
     );
+    
     $connection->process = proc_open(CMD, $descriptorspec, $pipes, null, $env);
+    
     $connection->pipes = $pipes;
     stream_set_blocking($pipes[0], 0);
+    
     $connection->process_stdout = new TcpConnection($pipes[1]);
-    $connection->process_stdout->onMessage = function($process_connection, $data)use($connection)
-    {
+    $connection->process_stdout->onMessage = function ($process_connection, $data) use ($connection) {
         $connection->send($data);
     };
-    $connection->process_stdout->onClose = function($process_connection)use($connection)
-    {
+    
+    $connection->process_stdout->onClose = function ($process_connection) use ($connection) {
         $connection->close();   //Close WebSocket connection on process exit.
     };
+    
     $connection->process_stdin = new TcpConnection($pipes[2]);
-    $connection->process_stdin->onMessage = function($process_connection, $data)use($connection)
-    {
+    $connection->process_stdin->onMessage = function ($process_connection, $data) use ($connection) {
         $connection->send($data);
     };
 };
 
-$worker->onMessage = function($connection, $data)
-{
-    if(ALLOW_CLIENT_INPUT)
-    {
+$worker->onMessage = function ($connection, $data) {
+    if (ALLOW_CLIENT_INPUT) {
         fwrite($connection->pipes[0], $data);
     }
 };
 
-$worker->onClose = function($connection)
-{
+$worker->onClose = function ($connection) {
     $connection->process_stdin->close();
     $connection->process_stdout->close();
     fclose($connection->pipes[0]);
@@ -90,10 +89,8 @@ $worker->onClose = function($connection)
     $connection->process = null;
 };
 
-$worker->onWorkerStop = function($worker)
-{
-    foreach($worker->connections as $connection)
-    {
+$worker->onWorkerStop = function ($worker) {
+    foreach ($worker->connections as $connection) {
         $connection->close();
     }
 };
@@ -101,7 +98,6 @@ $worker->onWorkerStop = function($worker)
 $webserver = new WebServer('http://0.0.0.0:7779');
 $webserver->addRoot('localhost', __DIR__ . '/Web');
 
-if(!defined('GLOBAL_START'))
-{
+if (!defined('GLOBAL_START')) {
     Worker::runAll();
 }
