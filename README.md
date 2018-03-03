3fd9f7a83b4ce3bb8dc23ee46c184b9785a77245
Lawrence Cherone <lawrence@cherone.co.uk>
2018-03-03 12:45:30 +0000

    Switched term.js to xterm.js, Changed it so it hooks directly into bash instead of a single process.

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # phptty
 A terminal in your browser using websocket php and  [workerman](https://github.com/walkor/Workerman), similar to [gotty](https://github.com/yudai/gotty).
 
+Forked Changes:
+ - Switched term.js to xterm.js
+ - Tidy some codez
+ - Changed it so it hooks directly into bash instead of a single process.
+
 # Screenshot
 ![Screenshot](https://github.com/walkor/phptty/blob/master/Web/imgs/example.gif?raw=true)
 
-# Live demo
-[Live demo](http://47.88.13.70:7779/)
-
 # install
-1. ```git clone https://github.com/walkor/phptty```
+1. ```git clone https://github.com/lcherone/phptty```
 2. ```cd phptty```
 3. ```composer install```
 
@@ -25,4 +27,3 @@ Visit ```http://ip:7779``` in your browser.
 [https://github.com/yudai/gotty](https://github.com/yudai/gotty)  
 [https://github.com/chjj/term.js](https://github.com/chjj/term.js)    
 [https://github.com/walkor/Workerman](https://github.com/walkor/Workerman)    
-
--- start.php
+++ start.php
@@ -17,7 +17,7 @@ use \Workerman\WebServer;
 use \Workerman\Connection\TcpConnection;
 
 // Command. For example 'tail -f /var/log/nginx/access.log'.
-define('CMD', 'htop');
+define('CMD', 'bash');
 
 // Whether to allow client input.
 define('ALLOW_CLIENT_INPUT', true);
@@ -31,57 +31,53 @@ $worker = new Worker("Websocket://0.0.0.0:7778");
 $worker->name = 'phptty';
 $worker->user = USER;
 
+// on connection
 $worker->onConnect = function ($connection) {
-    //To do this, PHP_CAN_DO_PTS must be enabled. See ext/standard/proc_open.c in PHP directory.
-    /*$descriptorspec = array(
-        0 => array('pty'),
-        1 => array('pty'),
-        2 => array('pty')
-    );*/
-    
-    //Pipe can not do PTY. Thus, many features of PTY can not be used.
-    //e.g. sudo, w3m, luit, all C programs using termios.h, etc.
-    $descriptorspec = [
-        0 => ["pipe", "r"],
-        1 => ["pipe", "w"],
-        2 => ["pipe", "w"]
-    ];
-    
+
     unset($_SERVER['argv']);
-    
-    $env = array_merge(
+
+    // initialise process
+    $connection->process = proc_open(CMD, [
+        0 => ["pty", "r"],
+        1 => ["pty", "w"],
+        2 => ["pty", "w"]
+    ], $pipes, realpath('./'), array_merge(
         ['COLUMNS' => 130, 'LINES' => 50], $_SERVER
-    );
-    
-    $connection->process = proc_open(CMD, $descriptorspec, $pipes, null, $env);
-    
+    ));
+
     $connection->pipes = $pipes;
     stream_set_blocking($pipes[0], 0);
-    
-    $connection->process_stdout = new TcpConnection($pipes[1]);
-    $connection->process_stdout->onMessage = function ($process_connection, $data) use ($connection) {
+
+    // stdout
+    $connection->stdout = new TcpConnection($pipes[1]);
+    //
+    $connection->stdout->onMessage = function ($process_connection, $data) use ($connection) {
         $connection->send($data);
     };
-    
-    $connection->process_stdout->onClose = function ($process_connection) use ($connection) {
-        $connection->close();   //Close WebSocket connection on process exit.
+    //
+    $connection->stdout->onClose = function ($process_connection) use ($connection) {
+        $connection->close(); // Close WebSocket connection on process exit.
     };
-    
-    $connection->process_stdin = new TcpConnection($pipes[2]);
-    $connection->process_stdin->onMessage = function ($process_connection, $data) use ($connection) {
+
+    // stdin
+    $connection->stdin = new TcpConnection($pipes[2]);
+    //
+    $connection->stdin->onMessage = function ($process_connection, $data) use ($connection) {
         $connection->send($data);
     };
 };
 
+// on websocket message
 $worker->onMessage = function ($connection, $data) {
     if (ALLOW_CLIENT_INPUT) {
         fwrite($connection->pipes[0], $data);
     }
 };
 
+// on websocket close
 $worker->onClose = function ($connection) {
-    $connection->process_stdin->close();
-    $connection->process_stdout->close();
+    $connection->stdin->close();
+    $connection->stdout->close();
     fclose($connection->pipes[0]);
     $connection->pipes = null;
     proc_terminate($connection->process);
@@ -89,12 +85,14 @@ $worker->onClose = function ($connection) {
     $connection->process = null;
 };
 
+// on worker stopped
 $worker->onWorkerStop = function ($worker) {
     foreach ($worker->connections as $connection) {
         $connection->close();
     }
 };
 
+// create webserver
 $webserver = new WebServer('http://0.0.0.0:7779');
 $webserver->addRoot('localhost', __DIR__ . '/Web');
 
